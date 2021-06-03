197bd87ced04a3bc6d211367875193fbfa00aea9
kamataryo <kamataryo@users.noreply.github.com>
2021-06-03 16:37:28 +0900

    Apply layers

---
+++ docs/layers.js
@@ -0,0 +1,51 @@
+export const clusterLayer = {
+  id: "spots-cluster-layer",
+  source: "spots",
+  type: "circle",
+  filter: ["has", "point_count"],
+  paint: {
+    "circle-color": "#FF0000",
+    "circle-radius": 20,
+    "circle-opacity": 1,
+  },
+};
+
+export const clusterCountLayer = {
+  id: "cluster-count-layer",
+  type: "symbol",
+  source: "spots",
+  filter: ["has", "point_count"],
+  layout: {
+    "text-field": "{point_count_abbreviated} 件",
+    "text-font": ["Noto Sans Regular"],
+    "text-size": 12,
+  },
+  paint: {
+    "text-color": "#FFFFFF",
+  },
+};
+
+export const spotsLayer = {
+  id: "spots-layer",
+  type: "circle",
+  source: "spots",
+  filter: ["all", ["==", "$type", "Point"], ["!has", "point_count"]],
+  paint: {
+    "circle-radius": 13,
+    "circle-color": "#FF0000",
+    "circle-opacity": 0.4,
+    "circle-stroke-width": 2,
+    "circle-stroke-color": "#FFFFFF",
+    "circle-stroke-opacity": 1,
+  },
+};
+
+export const labelLayer = {
+  id: "spots-label-layer",
+  type: "symbol",
+  source: "spots",
+  layout: {
+    "text-field": "{名称}",
+    "text-font": ["Noto Sans Regular"],
+  },
+};
--- docs/main.js
+++ docs/main.js
@@ -1,50 +1,24 @@
+import {
+  clusterLayer,
+  clusterCountLayer,
+  spotsLayer,
+  labelLayer,
+} from "./layers.js";
+import { csv2geojson } from "./util.js";
+
 const spreadsheetCSVExportUrl =
   "https://docs.google.com/spreadsheets/d/e/2PACX-1vScMm_fKfKVravIvXN3NnG9gRRdsti00wEWjTWfebqe8P9uxKMIsn5pcNE2dLDSf3ac8Udm3RydkMw0/pub?gid=0&single=true&output=csv";
 
 const fetchDataAsGeoJSON = () => {
   return fetch(spreadsheetCSVExportUrl)
     .then((res) => res.text())
-    .then((csv) => {
-      const [header, ...rows] = csv.split("\r\n");
-      return {
-        type: "FeatureCollection",
-        features: rows.map((row) => {
-          const keys = header.split(",");
-          const values = row.split(",");
-          return values.reduce(
-            (prev, value, index) => {
-              if (value === "") {
-                return prev;
-              } else {
-                const key = keys[index];
-                console.log(key, value);
-                if (key === "経度") {
-                  prev.geometry.coordinates[0] = parseFloat(value);
-                } else if (key === "緯度") {
-                  prev.geometry.coordinates[1] = parseFloat(value);
-                } else {
-                  prev.properties[key] = value;
-                }
-                return prev;
-              }
-            },
-            {
-              type: "Feature",
-              properties: {},
-              geometry: {
-                type: "Point",
-                coordinates: [0, 0],
-              },
-            }
-          );
-        }),
-      };
-    });
+    .then((csv) => csv2geojson(csv));
 };
 
 const main = async () => {
   const geojsonObject = await fetchDataAsGeoJSON();
   const map = new window.geolonia.Map("#map");
+
   map.addSource("spots", {
     type: "geojson",
     data: geojsonObject,
@@ -52,40 +26,27 @@ const main = async () => {
     clusterMaxZoom: 14,
     clusterRadius: 50,
   });
-  map.addLayer({
-    id: "spots-layer",
-    source: "spots",
-    type: "circle",
-    filter: ["has", "point_count"],
-    paint: {
-      "circle-color": [
-        "step",
-        ["get", "point_count"],
-        "#51bbd6",
-        100,
-        "#f1f075",
-        750,
-        "#f28cb1",
-      ],
-      "circle-radius": ["step", ["get", "point_count"], 20, 100, 30, 750, 40],
-    },
-  });
-  map.on("click", "clusters", function (e) {
-    alert(1);
-    const features = map.queryRenderedFeatures(e.point, {
-      layers: ["clusters"],
-    });
-    var clusterId = features[0].properties.cluster_id;
-    map
-      .getSource("spots")
-      .getClusterExpansionZoom(clusterId, function (err, zoom) {
-        if (err) return;
 
+  map.addLayer(clusterLayer);
+  map.addLayer(clusterCountLayer);
+  map.addLayer(spotsLayer);
+  map.addLayer(labelLayer);
+
+  map.on("click", "spots-cluster-layer", (e) => {
+    const features = map.queryRenderedFeatures(e.point, {
+      layers: ["spots-cluster-layer"],
+    });
+    const clusterId = features[0].properties.cluster_id;
+    map.getSource("spots").getClusterExpansionZoom(clusterId, (error, zoom) => {
+      if (error) {
+        return;
+      } else {
         map.easeTo({
           center: features[0].geometry.coordinates,
           zoom: zoom,
         });
-      });
+      }
+    });
   });
 };
 main();
